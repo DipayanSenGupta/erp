9c5daa05c18cd158f170c7052075ae5484909b18
Dipayan Sen Gupta <dip.himu@gmail.com>
2019-11-03 21:42:56 +0600

    Add auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to ERP Solution
                 </div>
 
                 <div class="links">
