ed499a0ca76ac98ed6291cbdac0d9f691af6b64a
Dipayan Sen Gupta <dip.himu@gmail.com>
2019-11-03 10:37:18 +0600

    Add customer migraiton,controller and model

---
+++ database/migrations/2019_10_27_140217_create_customers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('refNo');
+            $table->string('name');
+            $table->string('city');
+            $table->string('country');
+            $table->integer('telephone');
+            $table->integer('mobile');
+            $table->string('fax');
+            $table->string('contactPerson');
+            $table->string('emailId');
+            $table->mediumText('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
---
+++ database/seeds/EventTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Customer;
+class EventTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+     
+        }
+    }
+}
